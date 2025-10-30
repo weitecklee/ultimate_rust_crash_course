3e8f7930d62ce2813a22aea278225b667a9f2672
arkteck <arkteck@gmail.com>
2025-10-30 19:40:13 -0400

    exercise F done

--- exercise/f_strings/src/main.rs
+++ exercise/f_strings/src/main.rs
@@ -13,7 +13,7 @@
 fn main() {
     // 1. Using unicode escape codes, use println to print out a sparkles emoji (codepoint 2728).
 
-    // println!( ... );
+    println!("\u{2728}");
 
     // 2. Uncomment the commented line below and set the value of `favorite` to the emoji "🍓"
     // (codepoint 1f353).
@@ -22,8 +22,9 @@ fn main() {
     // - Use .to_string() to convert the string literal into a String.
 
     let mut favorite = String::new();
-    // favorite = ...
-    if favorite != "" {
+    // favorite = "\u{1f353}".to_string();
+    favorite = "🍓".to_string();
+    if !favorite.is_empty() {
         println!("Everyone's favorite fruit is: {favorite}");
     }
 
@@ -35,8 +36,13 @@ fn main() {
     // for all
     // great men
 
-    // let saying = "Now ...
-    // println!("{saying}");
+    // let saying = "Now is\nthe time\nfor all\ngreat men";
+    let saying = "\
+      Now is\n\
+      the time\n\
+      for all\n\
+      great men";
+    println!("{saying}");
 
     // Challenge: Change the string literal in #3 so that it:
     //
