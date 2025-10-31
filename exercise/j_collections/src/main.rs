49a8d618b9925c28fe3df6d04536c294c2f36345
arkteck <arkteck@gmail.com>
2025-10-31 14:31:48 -0400

    exercise J done

--- exercise/j_collections/src/main.rs
+++ exercise/j_collections/src/main.rs
@@ -9,33 +9,36 @@ fn main() {
     // this by creating `things` and then calling the `push` method repeatedly, or by using the
     // `vec!` macro. Then uncomment and run the code below.
 
-    // let things ...
-    // println!("{:?}", things); // `:?` means "the debug representation"
+    let mut things = vec![item, animal, container, material];
+    println!("{:?}", things); // `:?` means "the debug representation"
 
     // 2. Print out the length of the `things` vector using the `len` method.
 
-    // println!("things has a length of {}", ...);
+    println!("things has a length of {}", things.len());
 
     // 3. We want to use the `animal` variable in the (commented-out) code below, but we cannot
     // because the value has been moved into `things`. Uncomment the code below and change it to use
     // array indexing (with square brackets []) to index into `things` to access the `fox` String.
 
-    // println!("What does the {} say?", animal); // get the value from `things` instead of `animal`
+    println!("What does the {} say?", things[1]); // get the value from `things` instead of `animal`
 
     // 4. Sort `things` by calling the `sort` method. The variable needs to be mutable for this to
     // compile without errors. Then uncomment and run the code below.
 
-    // println!("Sorted values: {things:?}"); // variables can go inside the curly braces
+    things.sort();
+    println!("Sorted values: {things:?}"); // variables can go inside the curly braces
 
     // 5. Use a `for` loop to print out each item in `things`. It is okay to consume `things`, since
     // we won't be using it any more after this.
 
-    // for ...
+    for thing in things {
+        println!("{thing}");
+    }
 
     // Challenge: Create a vector named `buffer` containing 1024 zeroes using the `vec!` macro. This
     // should easily fit on one line without wrapping.
 
-    // let buffer = ...
+    let mut buffer = vec![0; 1024];
 
     // Challenge 2: Use a `for` loop and array indexing to change each entry in `buffer` to be its
     // index value multiplied by 2. For example:
@@ -47,5 +50,9 @@ fn main() {
     //
     // Then uncomment and run the code below.
 
-    // println!("Here's a buffer full of even values: {buffer:?}");
+    // for i in 0..buffer.len() {
+    for (i, item) in buffer.iter_mut().enumerate() {
+        *item = i * 2;
+    }
+    println!("Here's a buffer full of even values: {buffer:?}");
 }
